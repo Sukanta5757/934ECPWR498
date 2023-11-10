3a1df626e2f9a77380dcf08cd056aac939fe3430
Sukanta5757 <sukantanayak5757@gmail.com>
2023-11-10 21:55:11 +0530

    Benner.js and Benner.css content change.

--- amazon_clone_react/frontend/src/components/benner/Benner.js
+++ amazon_clone_react/frontend/src/components/benner/Benner.js
@@ -2,19 +2,38 @@ import '../benner/Benner.css';
 
 const Benner = ()=> {
     return(
-        // container
-    <div class="container">
-        {/* benner section */}
-        <div class="benner">
-            <div class="benner_image">
-                <img src="http://127.0.0.1:5500/Amazon_clone/images/benner1.jpg" alt=""></img>
-            </div>
-            <div class="button">
-                <button class="prev">&#10094;</button>
-                <button class="next">&#10095;</button>
-            </div>
+        // carousel container
+        <div class="carousel-container">
+        {/* carousel inner  */}
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img src="images/benner1.jpg" class="d-block w-100" alt="..."></img>
+          </div>
+          <div class="carousel-item">
+            <img src="images/benner2.jpg" alt=""></img>
+          </div>
+          <div class="carousel-item">
+            <img src="images/benner3.jpg" class="d-block w-100" alt="..."></img>
+          </div>
+          <div class="carousel-item">
+            <img src="images/benner4.jpg" alt=""></img>
         </div>
-    </div>
+        <div class="carousel-item">
+            <img src="images/benner5.jpg" alt=""></img>
+        </div>
+        <div class="carousel-item">
+            <img src="images/benner6.jpg" alt=""></img>
+        </div>
+        </div>
+        <div class="button-p-n">
+            <button class="carousel-control-prev hover" type="button" id="prev">
+                &#10094;
+            </button>
+            <button class="carousel-control-next hover" type="button" id="next">
+                &#10095;
+            </button>
+        </div>
+      </div>
     )
 }
 
