637995418a0a3076d928a13e3c9d59a2cb5cdf94
Sukanta5757 <sukantanayak5757@gmail.com>
2023-11-12 01:01:51 +0530

    error (export 'default' (imported as 'Benner') was not found in './components/benner/Benner' (module has no exports) )

--- amazon_clone_react/frontend/src/components/benner/Benner.js
+++ amazon_clone_react/frontend/src/components/benner/Benner.js
@@ -1,40 +1,10 @@
 import '../benner/Benner.css';
 
-const Benner = ()=> {
-    return(
-        // carousel container
-        <div class="carousel-container">
-        {/* carousel inner  */}
-        <div class="carousel-inner">
-          <div class="carousel-item">
-            <img src="images/benner1.jpg" class="d-block w-100" alt="..."></img>
-          </div>
-          <div class="carousel-item">
-            <img src="images/benner2.jpg" alt=""></img>
-          </div>
-          <div class="carousel-item">
-            <img src="images/benner3.jpg" class="d-block w-100" alt="..."></img>
-          </div>
-          <div class="carousel-item">
-            <img src="images/benner4.jpg" alt=""></img>
-        </div>
-        <div class="carousel-item">
-            <img src="images/benner5.jpg" alt=""></img>
-        </div>
-        <div class="carousel-item">
-            <img src="images/benner6.jpg" alt=""></img>
-        </div>
-        </div>
-        <div class="button-p-n">
-            <button class="carousel-control-prev hover" type="button" id="prev">
-                &#10094;
-            </button>
-            <button class="carousel-control-next hover" type="button" id="next">
-                &#10095;
-            </button>
-        </div>
-      </div>
-    )
-}
+const Benner = ()=>{
 
-export default Benner;
+  return(
+    <>
+    
+    </>
+  )
+}
