2ab1e68157dd3549c9f8e352f191ad6fcd505bba
Sukanta5757 <sukantanayak5757@gmail.com>
2023-11-15 13:42:17 +0530

    created slider benner in Benner.js . and style in Benner.css .

--- amazon_clone_react/frontend/src/components/benner/Benner.js
+++ amazon_clone_react/frontend/src/components/benner/Benner.js
@@ -1,10 +1,59 @@
+import React, {useEffect, useState } from 'react';
 import '../benner/Benner.css';
+import data from '../benner/CarouseImagel';
 
-const Benner = ()=>{
-
-  return(
-    <>
+const Benner = () => {
+    const [people] = useState(data);
+    const [index, setIndex] = useState(0);
     
-    </>
-  )
+    useEffect(()=>{
+        const lastIndex = people.length - 1;
+        if(index < 0) {
+            setIndex(lastIndex);
+        }
+        if(index> lastIndex) {
+            setIndex(0);
+        }
+    }, [index, people] );
+
+    useEffect(()=>{
+        let slider = setInterval(()=> {
+            setIndex(index + 1)
+        }, 5000);
+        return ()=>{
+            clearInterval(slider)
+        }
+    }, [index])
+
+    return (
+        <div className='carousel-container'>
+            <div className='carousel-inner'>
+                {people.map((item, indexPeople)=> {
+                    const {id, image, name, title, quote} = item;
+                    let position = "nextSlide";
+                    if(indexPeople === index) {
+                        position = "actveSlide"
+                    }
+                    if(indexPeople === index -1 || (index === 0 && indexPeople === people.length - 1)){
+                        position = "lastSlide";
+                    }
+                    return (
+                        <article className={position} key={id}>
+                            <img src={image} alt={name} className='carousel_item'/>
+                        </article>
+                    )
+                })}
+                
+            </div>
+
+            <button className="prev" onClick={()=> setIndex(index - 1)}>
+                &#10094;
+            </button>
+            <button className="next" onClick={()=> setIndex(index + 1)}>
+                &#10095; 
+            </button>
+        </div>
+    )
 }
+
+export default Benner;
---
+++ amazon_clone_react/frontend/src/components/benner/CarouseImagel.js
@@ -0,0 +1,50 @@
+
+const people = [
+    {
+        id: 1,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner1.jpg",
+        name: "Harry stamper",
+        title: "manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: 2,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner2.jpg",
+        name: "james gun",
+        title: "product manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: 3,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner3.jpg",
+        name: "amit gun",
+        title: "product manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: 4,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner4.jpg",
+        name: "Harry stamper",
+        title: "manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: 5,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner2.jpg",
+        name: "james gun",
+        title: "product manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: 6,
+        image: "http://127.0.0.1:5500/Amazon_clone/images/benner3.jpg",
+        name: "amit gun",
+        title: "product manager",
+        quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }
+
+
+
+]
+
+export default people;
