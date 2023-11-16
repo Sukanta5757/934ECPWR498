aec7bdde67a5970d1f1905ed6f2b7a66bf862ddc
Sukanta5757 <sukantanayak5757@gmail.com>
2023-11-16 11:42:38 +0530

    created home product cards and style in Homepage.js and Homepage.css

--- amazon_clone_react/frontend/src/App.js
+++ amazon_clone_react/frontend/src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './components/header/Navbar';
 import Benner from './components/benner/Benner';
+import Homepage from './components/homepage/Homepage';
+
 
 function App() {
   return (
     <>
       <Navbar/>
       <Benner/>
+      <Homepage/>
     </>
   );
 }
---
+++ amazon_clone_react/frontend/src/components/homepage/Homepage.js
@@ -0,0 +1,226 @@
+
+// import React from "react";
+import '../homepage/Homepage.css';
+import data from '../benner/CarouseImagel';
+
+const Homepage = ()=> {
+
+    return (
+    <main class="main_content">
+        {/* product cards */}
+        <div class="card-product-content">
+           <div class="card-product">
+                <h2>Deals on smartphones that suits your budget</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/cardmob1.jpg"></img>
+                        <p>Budget | Under ₹10,000</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/cardmob2.jpg"></img>
+                        <p>Mid range | ₹10,000 - ₹25,000</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/cardmob3.jpg"></img>
+                        <p>Premium | ₹25,000 - ₹40,000</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/cardmob4.jpg"></img>
+                        <p>Ultra premium | Above ₹40,000</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div> 
+            
+            <div class="card-product">
+                <h2>Up to 75% off | Headphones</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card1.jpg"></img>
+                        <p>boAt Up to 75% off | boAt</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card2.jpg"></img>
+                        <p>boAtUp to 75% off | boult</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card3.jpg"></img>
+                        <p>boAtUp to 75% off | noise</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card4.jpg"></img>
+                        <p>boAtUp to 75% off | zebronics</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+
+            <div class="card-product">
+                <h2>Up to 60% off | Styles for men</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card5.jpg"></img>
+                        <p>Clothing</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card6.jpg"></img>
+                        <p>Footwear</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card7.jpg"></img>
+                        <p>Watches</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card8.jpg"></img>
+                        <p>Bags & wallets</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+
+            <div class="card-product">
+                <h2>Revamp your home in style</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card9.jpg"></img>
+                        <p>Bedsheets, curtains & more</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card10.jpg"></img>
+                        <p>Home decoration</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card11.jpg"></img>
+                        <p>Home storage</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card12.jpg"></img>
+                        <p>Lighting solutions</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+        </div>
+
+        <div class="card-product-content">
+            <div class="card-product">
+                <h2>Appliances for your home | Up to 55% off</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card13.jpg"></img>
+                        <p>Air conditioners</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card14.jpg"></img>
+                        <p>Refrigerators</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card15.jpg"></img>
+                        <p>Microweves</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card16.jpg"></img>
+                        <p>Washising machines</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+
+            <div class="card-product">
+                <h2>Revamp your home in style</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card9.jpg"></img>
+                        <p>Bedsheets, curtains & more</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card10.jpg"></img>
+                        <p>Home decoration</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card11.jpg"></img>
+                        <p>Home storage</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card12.jpg"></img>
+                        <p>Lighting solutions</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+
+            <div class="card-product">
+                <h2>Up to 60% off | Styles for men</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card5.jpg"></img>
+                        <p>Clothing</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card6.jpg"></img>
+                        <p>Footwear</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card7.jpg"></img>
+                        <p>Watches</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card8.jpg"></img>
+                        <p>Bags & wallets</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+
+            <div class="card-product">
+                <h2>Up to 75% off | Headphones</h2>
+                <div class="product-nested-cards">
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card1.jpg"></img>
+                        <p>boAt Up to 75% off | boAt</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card2.jpg"></img>
+                        <p>boAtUp to 75% off | boult</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card3.jpg"></img>
+                        <p>boAtUp to 75% off | noise</p>
+                    </div>
+
+                    <div class="nested-card">
+                        <img src="http://127.0.0.1:5500/Amazon_clone/images/card4.jpg"></img>
+                        <p>boAtUp to 75% off | zebronics</p>
+                    </div>
+                </div>
+                <button class="card-product-btn">see more</button>
+            </div>
+        </div>
+    </main>
+    )
+}
+
+export default Homepage ;
